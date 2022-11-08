27f5b60abcc3f7933c7bec585aec6988599b822d
Jason Penilla <11360596+jpenilla@users.noreply.github.com>
2022-11-07 18:43:35 -0700

    Update run-paper and paperweight

--- test-plugin/build.gradle.kts
+++ test-plugin/build.gradle.kts
@@ -1,7 +1,7 @@
 plugins {
   `base-conventions`
   id("io.papermc.paperweight.userdev")
-  id("xyz.jpenilla.run-paper") version "1.0.6"
+  id("xyz.jpenilla.run-paper") version "1.1.0"
   id("net.minecrell.plugin-yml.bukkit") version "0.5.1"
   id("com.github.johnrengelman.shadow") version "7.1.2"
 }
